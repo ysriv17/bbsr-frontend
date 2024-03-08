47ca5a8e00b65ef884c3503a0b79e016dee8a9a9
ysriv17 <138852360+ysriv17@users.noreply.github.com>
2024-03-08 13:24:55 +0530

    Update LoginPage.jsx

--- src/components/loginpage/LoginPage.jsx
+++ src/components/loginpage/LoginPage.jsx
@@ -53,7 +53,6 @@ export default function LoginPage() {
                 {
                     headers: {
                         'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin':'*',
                         'Access-Control-Allow-Origin' : 'https://eximlogistics.netlify.app/'
 
                     }
